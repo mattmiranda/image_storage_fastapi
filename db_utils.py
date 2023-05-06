f9d75b7a88e71498eae6bdd8c5d4c731e7a815df
Matheus Miranda <matheusfariasmiranda@gmail.com>
2023-05-06 14:36:39 +0900

    [#FIX] Remove all BaseModel dependency, using pure python dict, list and json

--- db_utils.py
+++ db_utils.py
@@ -3,7 +3,7 @@
 import json
 from typing import Optional
 
-def read(filename: Optional[str] = 'database.json'):
+def read(filename: Optional[str] = 'database.json') -> list:
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
 
@@ -11,11 +11,26 @@ def write(data: list, filename: Optional[str] = 'database.json'):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
+def add_image(data: dict):
+    images_list = read()
+    images_list.append(data)
+    write(images_list)
+
+def remove_image(image_id: str):
+    images = list_to_dict(read())
+    image = images.pop(image_id)
+    write(dict_to_list(images))
+    return image
+
 def list_to_dict(data: list):
-    print("hello")
-    return {}
+    d = {}
+    for item in data:
+        print(item)
+        d[item['_id']] = item
+    return d
 
 def dict_to_list(data: dict):
-    print("hello")
-    return []
-
+    l = []
+    for item in data.values():
+        l.append(item)
+    return l
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 import uuid
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -7,12 +8,12 @@ import db_utils as db
 app = FastAPI()
 
 # I understand that pydantic is very useful to create the data models. 
-# I will try using it for data validation. 
-class Image(BaseModel):
-    _id: str
-    createdDate: int
-    filename: str
-    contentType: str
+# I will try using it for data validation later.
+# class Image(BaseModel):
+#     _id: str
+#     createdDate: int
+#     filename: str
+#     contentType: str
 
 @app.get("/")
 def index():
@@ -22,6 +23,8 @@ def index():
 
 @app.get("/image/{image_id}")
 def query_image_by_id(image_id: str):
+    images_list = db.read()
+    images = db.list_to_dict(images_list)
     if image_id not in images:
         raise HTTPException(status_code=404, detail=f"Image with ID {image_id} does not exist")
     else:
@@ -30,40 +33,44 @@ def query_image_by_id(image_id: str):
 # Problem with the typing Union operator in python 3.9
 @app.get("/image")
 def query_image_by_parameter(
-        filename: str
-        # contentType: Optional[str],
-        # createdDate: Optional[int]
+        filename: Optional[str] = None,
+        contentType: Optional[str] = None,
+        createdDate: Optional[int] = None
 ):
-    def check_image(image: Image):
-        return filename is None or image.filename == filename
-        # return all(
-        #     (
-        #         filename is None or image.filename == filename
-        #         contentType is None or image.contentType == contentType,
-        #         createdDate is None or image.createdDate == createdDate
-        #     )
-        # )
-    selection = [image for image in images.values() if check_image(image)]
+    def check_image(image: dict): #(image: Image)
+        return all(
+            (
+                filename is None or image['filename'] == filename,
+                contentType is None or image['contentType'] == contentType,
+                createdDate is None or image['createdDate'] == createdDate
+            )
+        )
+    images_list = db.read()
+    selection = [image for image in images_list if check_image(image)]
     return {
-        "query": {"filename": filename},
+        "query": {"filename": filename, "contentType": contentType, "createdDate": createdDate},
         "selection": selection
     }
 
 @app.post("/")
-def add_image(image: Image):
-    for img in images:
-        if image.filename == img:
-            HTTPException(status_code=400, detail=f"Image with id {image._id} already exists")
+def add_image(image: dict): #(image: Image)
+    images_list = db.read()
+    for img in images_list:
+        if image["filename"] == img["filename"]:
+            HTTPException(status_code=400, detail=f"Image with same name already exists")
 
-    images[image._id] = image
+    image['_id'] = uuid.uuid1().hex
+    result = db.add_image(image)
     return {"status": "success", "added_image": image}
     
-# @app.put("/images/{image_id}")
+# # @app.put("/images/{image_id}")
     
 @app.delete("/image/{image_id}")
 def delete_image(image_id: str):
+    images_list = db.read()
+    images = db.list_to_dict(images_list)
     if image_id not in images:
         raise HTTPException(status_code=404, detail=f"Image with id {image_id} does not exist")
     
-    image = images.pop(image_id)
+    image = db.remove_image(image_id)
     return {"status": "success", "delete_image": image}
