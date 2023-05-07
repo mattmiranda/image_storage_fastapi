771ef31a0313f2fda8951ccd0f7b0a39cd167bf7
Matheus Miranda <matheusfariasmiranda@gmail.com>
2023-05-08 01:44:15 +0900

    [#FIX] Formatting

--- db_utils.py
+++ db_utils.py
@@ -4,18 +4,22 @@ import json
 from datetime import datetime
 from typing import Optional
 
-def read(filename: Optional[str] = 'database.json') -> list:
-    with open(filename, 'r', encoding='utf-8') as f:
+
+def read(filename: Optional[str] = "database.json") -> list:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
 
-def write(data: list, filename: Optional[str] = 'database.json'):
-    with open(filename, 'w', encoding='utf-8') as f:
+
+def write(data: list, filename: Optional[str] = "database.json"):
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+
 
 def add_image(data: dict):
     images_list = read()
     images_list.append(data)
     write(images_list)
+
 
 def remove_image(image_id: str):
     images = list_to_dict(read())
@@ -23,21 +27,24 @@ def remove_image(image_id: str):
     write(dict_to_list(images))
     return image
 
+
 def update_entry(entry: dict):
     images = list_to_dict(read())
-    images[entry['_id']] = {
-        "_id": entry['_id'],
+    images[entry["_id"]] = {
+        "_id": entry["_id"],
         "createdDate": int(datetime.now().timestamp()),
-        "filename": entry['filename'],
-        "contentType": entry['contentType']
+        "filename": entry["filename"],
+        "contentType": entry["contentType"],
     }
     write(dict_to_list(images))
-    
+
+
 def list_to_dict(data: list):
     d = {}
     for item in data:
-        d[item['_id']] = item
+        d[item["_id"]] = item
     return d
+
 
 def dict_to_list(data: dict):
     l = []
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import db_utils as db
 
-IMG_BASE_PATH = './image/'
+IMG_BASE_PATH = "./image/"
 
 app = FastAPI()
 
@@ -26,9 +26,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# I understand that pydantic is very useful to create and manage the data models, 
-# however I decided to use only basic python objects and data types as the purpose 
+# I understand that pydantic is very useful to create and manage the data models,
+# however I decided to use only basic python objects and data types as the purpose
 # of this assignment is not to test my ability using pydantic.
+
 
 # Index route returns a list of all image objects
 @app.get("/")
@@ -36,26 +37,29 @@ def query_all_images() -> dict[str, list]:
     images = db.read()
     return {"images": images}
 
+
 # Query images by parameters included in the url. These are optional parameters.
 # Example: /image?filename=cat1.jpeg
 # TODO: Update python to 3.10+ in order to use simplified Optional/Union syntax, ie: str | None = None
 @app.get("/image")
 def query_image_by_parameter(
-        filename: Optional[str] = None,
-        contentType: Optional[str] = None,
-        createdDate: Optional[int] = None
+    filename: Optional[str] = None,
+    contentType: Optional[str] = None,
+    createdDate: Optional[int] = None,
 ) -> dict[str, list]:
     def check_image(image: dict):
         return all(
             (
-                filename is None or image['filename'] == filename,
-                contentType is None or image['contentType'] == contentType,
-                createdDate is None or image['createdDate'] == createdDate
+                filename is None or image["filename"] == filename,
+                contentType is None or image["contentType"] == contentType,
+                createdDate is None or image["createdDate"] == createdDate,
             )
         )
+
     images_list = db.read()
     selection = [image for image in images_list if check_image(image)]
     return {"query_result": selection}
+
 
 # API to get image by image_id. Returns image file.
 @app.get("/image/{image_id}")
@@ -63,9 +67,12 @@ def get_image_by_id(image_id: str) -> FileResponse:
     images_list = db.read()
     images = db.list_to_dict(images_list)
     if image_id not in images:
-        raise HTTPException(status_code=404, detail=f"Image with ID {image_id} does not exist")
+        raise HTTPException(
+            status_code=404, detail=f"Image with ID {image_id} does not exist"
+        )
 
-    return FileResponse(IMG_BASE_PATH + images[image_id]['filename'])
+    return FileResponse(IMG_BASE_PATH + images[image_id]["filename"])
+
 
 # Endpoint to add an image to storage and DB. Returns the downsized image file.
 @app.post("/")
@@ -75,14 +82,14 @@ async def add_image(file: Annotated[UploadFile, File()]) -> FileResponse:
 
     # Save image to 'image' directory using buffer
     out_file_path = IMG_BASE_PATH + file.filename
-    async with aiofiles.open(out_file_path, 'wb') as out_file:
-        while content := await file.read(5*1024):  # async read chunk
+    async with aiofiles.open(out_file_path, "wb") as out_file:
+        while content := await file.read(5 * 1024):  # async read chunk
             await out_file.write(content)  # async write chunk
 
     # If image is larger than 1MB, downsize it and overwrite the original image
     img = Image.open(out_file_path)
     if img.width > 500 or img.height > 500:
-        img.thumbnail((500,500))
+        img.thumbnail((500, 500))
     img.save(IMG_BASE_PATH + file.filename)
 
     images_list = db.read()
@@ -91,7 +98,7 @@ async def add_image(file: Annotated[UploadFile, File()]) -> FileResponse:
             # Image with same name already exists, update existing entry
             db.update_entry(img)
             return FileResponse(IMG_BASE_PATH + file.filename)
-        
+
     # Store image entry in DB
     new_image = {
         "_id": uuid.uuid1().hex,
@@ -102,21 +109,25 @@ async def add_image(file: Annotated[UploadFile, File()]) -> FileResponse:
     result = db.add_image(new_image)
     return FileResponse(IMG_BASE_PATH + file.filename)
 
+
 # TODO: Implement patching function for completeness
 # @app.put("/images/{image_id}")
 
-# Delete image data from DB and image file. 
+
+# Delete image data from DB and image file.
 @app.delete("/image/{image_id}")
 def delete_image(image_id: str):
     images_list = db.read()
     images = db.list_to_dict(images_list)
     if image_id not in images:
-        raise HTTPException(status_code=404, detail=f"Image with id {image_id} does not exist")
+        raise HTTPException(
+            status_code=404, detail=f"Image with id {image_id} does not exist"
+        )
 
     # Delete image entry from DB
     image = db.remove_image(image_id)
 
     # Delete image file from 'image' directory
-    os.remove(IMG_BASE_PATH + image['filename'])
+    os.remove(IMG_BASE_PATH + image["filename"])
 
     return {"status": "success", "delete_image": image}
